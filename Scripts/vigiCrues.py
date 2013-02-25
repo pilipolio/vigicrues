de352508980f0fd72ce40c1048692f4802443338
pilipolio <guillaume.allain@gmail.com>
2013-02-25 09:10:26 +0100

    Started missing values and distribution analysis.

--- Scripts/vigiCrues.py
+++ Scripts/vigiCrues.py
@@ -6,7 +6,18 @@
 # http://www.geolittoral.equipement.gouv.fr/comment-acceder-aux-donnees-via-r39.html
 # http://eductice.ens-lyon.fr/EducTice/projets/en-cours/geomatique/veille/sig/Jeux-de-donnees
 
+"""
+TODO :
+ * Start a notebook
+ * analyze and fill missing values
+ * calibration of heights/flows
+ * animatation of plots
+ * match station to stream and geometry
+ * build stream deformation according to flows/heights
+ * flow/height relationship
+"""
 from datetime import datetime
+from datetime import time
 import pickle
 import numpy as np
 from matplotlib import pyplot as plt
@@ -16,15 +27,6 @@ import pandas as pd
 directory = r'D:\PythonWorkspace\site_packages\vigicrues\Data'
 scraper = scraping.scraper(directory)
 stations = scraper.load_stations()
-
-## Querying flow and heights for all stations
-flows_by_ids = dict()
-heights_by_ids = dict()
-
-for s in stations[0:10] :
-    flows_by_ids[s['id']] = scraping.get_flows(s['id'])
-    heights_by_ids[s['id']] = scraping.get_heights(s['id'])
-    print '{} heights and {} flows fetched for station {}'.format(len(heights_by_ids[s['id']]), len(flows_by_ids[s['id']]), s['id'])
 
 heights = pd.DataFrame.from_dict(dict((id,dict(hs)) for id, hs in heights_by_ids.iteritems()),orient='columns')
 flows = pd.DataFrame.from_dict(dict((id,dict(hs)) for id, hs in flows_by_ids.iteritems()),orient='columns')
@@ -36,6 +38,28 @@ store['heights'] = heights
 
 flows = store['flows']
 heights = store['heights']
+
+# Missing data analysis : stations have a sampling period of 15/30/60min
+count_by_times = flows.groupby(lambda ts : ts.time()).count().sum(axis=1)
+plt.plot(count_by_times.index, count_by_times, color='grey',alpha=.8)
+t_60mins = [t for t in count_by_times.index if t.minute == 0]
+plt.plot(t_60mins, count_by_times[t_60mins],label='T=60  min')
+t_30mins = [t for t in count_by_times.index if t.minute == 30]
+plt.plot(t_30mins, count_by_times[t_30mins],label='T=30  min')
+t_15_mins = [t for t in count_by_times.index if t.minute in [15,45]]
+plt.plot(t_15_mins, count_by_times[t_15_mins],label='T=15  min')
+plt.xticks([t for t in count_by_times.index if t.minute == 0],['{:%H}'.format(t) for t in count_by_times.index if t.minute == 0])
+plt.legend()
+plt.savefig('Sampling_periods.png')
+plt.close()
+
+# Re-indexing flows and heights to match the 60 min period and forward fill missing value with h = 
+#flows = flows.reindex([t for t in flows.index if t.minute == 0])
+flows = flows.resample('H',how='mean')
+flows.fillna(method='ffill',limit=5,inplace=True)
+#heights = heights.reindex([t for t in heights.index if t.minute == 0])
+heights = heights.resample('H',how='mean')
+heights.fillna(method='ffill',limit=5,inplace=True)
 
 # Plot of flows and heights for a given station
 station_id = 1516
@@ -49,30 +73,43 @@ fs = flows[station_id][flows[station_id]>0]
 p_f = ax_f.plot(fs.index,fs,color='red',lw=2,alpha=.7,label='Flows')
 ax_f.legend(loc=1)
 for tick in ax_h.xaxis.get_major_ticks() : tick.label1.set_fontsize(6); tick.label1.set_rotation(30)
+plt.savefig('station_heights_and_flows.png')
 
-# plot all stations with size depending on flows
+# Map of all stations with size depending on flows
 import operator
 import itertools
 for stream,stream_stations in itertools.groupby(sorted(stations,key=operator.itemgetter('stream')),operator.itemgetter('stream')):
     plt.plot(*map(list,zip(*[(s['X'],s['Y']) for s in stream_stations])),color='grey',alpha=.25)
 
-xys = [(s['X'],s['Y']) for s in stations]
 cmap = mpl.ColorMap()
-dt = pd.datetime(2013,02,06)
+
+xys = [(s['X'],s['Y']) for s in stations]
+dt = pd.datetime(2013,02,12)
 plt.scatter(*zip(*xys),s=flows.ix[dt],lw=0,c=np.log(heights.ix[dt]),alpha=.75)
 plt.xticks([])
 plt.yticks([])
 plt.colorbar()
+ax = plt.gca()
+plt.setp(ax, frame_on=False)
+plt.savefig('{:%Y%m%d}_France_maps.png'.format(dt))
+
+# Bi-plot of flows/heights
+#flows.ix[union_index].values.shape
+#heights.ix[union_index].values.shape
+union_index = flows.index.union(heights.index)
+f_s = flows.ix[union_index].values
+h_s = heights.ix[union_index].values
+not_nan_idx = np.where(np.logical_not(logical_or(np.isnan(f_s),np.isnan(h_s))))
+
+plt.scatter(f_s[not_nan_idx],h_s[not_nan_idx],alpha=.05,s=.3,c=not_nan_idx[1])
+plt.ylim([0,15])
+plt.xlim([0,2000])
 
 # heights histogram
-plt.hist(hs,bins=10**np.linspace(-2, 2, 40))
+plt.hist(h_s[np.logical_not(np.isnan(h_s))].flat,bins=10**np.linspace(-2, 3, 60))
 plt.xscale('log')
 plt.close()
 
-scaleBins = np.array([-10000,0,0.1,0.3,1,3,10,30,100,10000])
-scaleIntervals = np.array([np.array([b1,b2]) for b1, b2 in zip(scaleBins[:-1],scaleBins[1:])])
-hsByInt = [[h for h in hs if (h >= b1 and h<b2)] for b1,b2 in scaleIntervals]
-
-plt.hist(hsByInt,bins=10**np.linspace(-2, 2, 40)),labels=scaleIntervals)
+plt.hist(f_s[np.logical_not(np.isnan(f_s))].flat,bins=10**np.linspace(-2, 5, 140))
 plt.xscale('log')
 plt.close()
