e66c9a0d4bfb7a4e35908aa11f58b2c719eaaa17
pilipolio <guillaume@oxfordian.(none)>
2013-07-27 23:19:25 +0100

    First wire up of stations into flask api.

--- src/web/app.py
+++ src/web/app.py
@@ -1,20 +1,23 @@
 from flask import Flask
 from flask import jsonify
 
+import scraping
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return 'Scrapped Vigicrues data rest API'
 
 @app.route('/stations')
 def get_stations():
-    return jsonify(station_ids=[1,2])
+    scraper = scraping.Scraper('../data/')  
+    return jsonify(ids=[s['id'] for s in scraper.load_stations()])
 
 @app.route('/station/<int:station_id>')
 def get_station(station_id):
-    return jsonify(id=station_id,
-                   name='Garigliano')
+    scraper = scraping.Scraper('../data/')
+    return jsonify([station for station in scraper.load_stations() if station['id'] == station_id][0])
 
 if __name__ == '__main__':
     app.run(debug=True)
