03aa4e3188e084017690592b773e6009a428ddaf
pilipolio <guillaume@oxfordian.(none)>
2013-07-28 22:10:16 +0100

    1st flow plot with dynamic data.

--- src/web/app.py
+++ src/web/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, make_response
 
+from datetime import datetime, timedelta 
+import time
+import json
 import scraping
 
 app = Flask(__name__)
@@ -10,7 +13,16 @@ def index():
 
 @app.route('/stationFlowGraph/<int:station_id>')
 def get_station_flow_graph(station_id):
-    return render_template('station_flow_graph.html')
+    return render_template('station_flow_graph.html', station_id=station_id)
+
+@app.route('/stationFlows/<int:station_id>')
+def get_station_flows(station_id):
+    time_y_tuples = scraping.get_flows(station_id)
+    data = [dict(x=(time.mktime(t.timetuple())), y=flow) for t, flow in time_y_tuples]
+    # As advertised here, cannot use jsonify to return array
+    # https://github.com/mitsuhiko/flask/issues/510
+    #return jsonify([{'name': str(station_id), 'data': data}])
+    return make_response(json.dumps([{'name': str(station_id), 'data': data}]))
 
 @app.route('/stations')
 def get_stations():
